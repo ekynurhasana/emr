dd8d59dc43a304caa01575b30c1180a6ad8037d5
Eky Arjayanto Nurhasana <eky.arjayanto@hashmicro.com.sg>
2023-09-09 20:06:52 +0800

    [TRY] fixing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -170,7 +170,7 @@ class UserController extends Controller
             $file_name = time() . '-' . $request->nama_foto;
             // save to public path
             $file_path = asset('/asset/img/profile_users/' . $file_name);
-            file_put_contents($file_path, $file);
+            fwrite(fopen($file_path, 'w'), $file);
             // update session detail_user->file_foto
             $data_detail = [
                 'nama_lengkap' => $request->nama_user,
